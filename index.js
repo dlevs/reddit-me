598aa960d046440633f08cafdef6cad90c7e33c0
Daniel Levett <dlevett@hotmail.co.uk>
2017-07-23 12:30:36 +0100

    Fix bug where some subreddits don’t work, and add comments

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const sample = require('lodash/sample');
 const args = require('./lib/args');
 const getters = require('./lib/responseGetters');
 const urls = require('./lib/urlCreators');
+const {redditKinds} = require('./lib/constants');
 
+/**
+ * Make a GET reqest to the reddit API at the relative path provided.
+ *
+ * @param {String} path
+ * @returns {Promise}
+ */
 const apiRequest = async (path) => {
 	const response = await fetch(
 		urls.roots.api(path),
@@ -16,30 +23,38 @@ const apiRequest = async (path) => {
 	return response.json();
 };
 
-const getRedditPostAndNotify = async () => {
-	const url = urls.paths.randomPost(sample(args.subreddits));
-	const posts = await apiRequest(url);
-	const post = getters.posts.getOriginalPost(posts);
+/**
+ * Show a notification with information from a random post from a random
+ * subreddit.
+ *
+ * @param {Array} [subreddits]
+ * @returns {Promise}
+ */
+const getRedditPostAndNotify = async (subreddits = []) => {
+	const url = urls.paths.randomPost(sample(subreddits));
+	const response = await apiRequest(url);
+	const post = getters.response.getRandomItemOfKind(response, redditKinds.LINK);
 
 	notifier.notify(
 		{
-			title: getters.post.getTitle(post),
-			subtitle: getters.post.getSubreddit(post),
+			title: getters.link.getTitle(post),
+			subtitle: getters.link.getSubreddit(post),
 			message: [
-				getters.post.getCommentCountText(post),
-				getters.post.getUpvoteCountText(post)
+				getters.link.getCommentCountText(post),
+				getters.link.getUpvoteCountText(post)
 			].join('\n'),
-			contentImage: getters.post.getThumbnail(post),
-			open: getters.post.getLink(post),
+			contentImage: getters.link.getThumbnail(post),
+			open: getters.link.getLink(post),
 			wait: true,
 			actions: ['Again!']
 		},
 		(err, type, {activationValue}) => {
 			if (activationValue === 'Again!') {
-				getRedditPostAndNotify();
+				getRedditPostAndNotify(subreddits);
 			}
 		}
 	);
 };
 
-getRedditPostAndNotify();
+// Init with arguments from the command line.
+getRedditPostAndNotify(args.subreddits);
